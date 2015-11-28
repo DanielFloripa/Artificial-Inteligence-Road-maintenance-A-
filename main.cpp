31de700bcbc943243adab5d3a89f00ae1e0678a0
André Nunes <dekonunesss@gmail.com>
2015-11-28 11:55:35 -0200

    Test commit by eclipse

--- main.cpp
+++ main.cpp
@@ -11,10 +11,6 @@ int main(int argc, char **argv) {
 	//int tipoBusca = atoi(argv[1]);
 	vector<vector<int> > matriz;
 	createLand(&matriz);
-	//showLand(matriz);
-	//buscaLargura(&matriz,10,10,20,20);
-	//buscaProfundidade(&matriz,10,10,20,20);
-	//buscaCustoUniforme(&matriz,10,10,20,20);
 	buscaA(&matriz,10,10,20,20);
 	showLand(matriz);
 	interfaceGrafica(matriz);
